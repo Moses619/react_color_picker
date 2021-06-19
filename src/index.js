8cd4560bad26a8f3dd6aaac155b7bccb25b84649
moses <ukwenyamoses8@gmail.com>
2021-06-19 13:48:37 -0700

    Added files via Upload.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import Bgcolor from './Bgcolor';
 import reportWebVitals from './reportWebVitals';
 
